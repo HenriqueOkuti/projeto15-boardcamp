9f05495073a42bf2912147809173f92f8174ea2f
HenriqueOkuti <henrique.krastins@gmail.com>
2022-10-03 12:24:06 -0300

    Fix: Adjusted connection for project grading

--- source/db/connection.js
+++ source/db/connection.js
@@ -3,11 +3,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const connection = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '12345',
-  database: 'boardcamp',
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default connection;
